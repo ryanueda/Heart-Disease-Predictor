94fb29c02f6e32087e0345fef1a4cd371a6a69e4
Ryan Teo <2b05.2122924.ryanueda@gmail.com>
2022-11-29 00:12:59 +0800

    Added prediction history and record deletion utility

--- application/routes.py
+++ application/routes.py
@@ -19,7 +19,7 @@ from application.forms import PredictionForm
 @app.route('/home')
 def index_page():
     form1 = PredictionForm()
-    return render_template("index.html", form=form1, title="Enter Parameters For Prediction")
+    return render_template("index.html", form=form1, entries=get_entries(), predict_type=predict_type, title="Enter Parameters For Prediction")
 
 ## predict route
 from flask import render_template, request, flash
@@ -55,10 +55,19 @@ def predict():
             add_entry(new_entry)
             
             flash(f"Prediction: {predict_type[int(result[0])]}", "success")
-            return render_template("index.html", title="Enter Parameters For Prediction", form=form, index=True )
+            return render_template("index.html", title="Enter Parameters For Prediction", form=form, index=True, entries=get_entries(), predict_type=predict_type )
         else:
             flash("Error, cannot proceed with prediction","danger")
             return render_template("index.html", title="Enter Parameters For Prediction", form=form, index=True )
+
+@app.route('/remove', methods=['POST'])
+def remove():
+    form = PredictionForm()
+    req = request.form
+    id = req["id"]
+    remove_entry(id)
+    return render_template("index.html", title="Enter Iris Parameters",
+    form=form, entries = get_entries(), index=True, predict_type=predict_type)
 
         
 def add_entry(new_entry):
@@ -69,3 +78,25 @@ def add_entry(new_entry):
     except Exception as error:
         db.session.rollback()
         flash(error,"danger")
+
+def get_entries():
+    try:
+        # entries = Entry.query.all() # version 2
+        entries = db.session.execute(db.select(Entry).order_by(Entry.id)).scalars()
+        return entries
+    except Exception as error:
+        db.session.rollback()
+        flash(error,"danger")
+        return 0
+
+# Added in Practical 5
+def remove_entry(id):
+    try:
+        # entry = Entry.query.get(id) # version 2
+        entry = db.get_or_404(Entry, id)
+        db.session.delete(entry)
+        db.session.commit()
+    except Exception as error:
+        db.session.rollback()
+        flash(error,"danger")
+        return 0
